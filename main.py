d86fff6c722936520389880f9a8e5c5c7ee1e4d8
MCLaptop <beliar359@gmail.com>
2025-03-11 11:56:05 +0100

    Setting up requirements and main.py file

---
+++ main.py
@@ -0,0 +1,11 @@
+# Using Python 3.12.3
+# Dependencies defined in requirements.txt were installed via command "pip install -r requirements.txt"
+# Project uses venv via "python3 -m venv venv" -> "source venv/bin/activate"
+import pygame
+
+
+
+
+
+if __name__ == "__main__":
+    main()
